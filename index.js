4f2b95323d1eb48a8d7999b517665dc4eb4a6882
ABHISHEK RAWAT <abhirawat623@gmail.com>
2023-08-07 23:27:33 +0530

    auth login

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 
 const PORT = 3000;
 
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+
+const jwtKey = "abhi"
+
+const userdata = require("./db/users")
 
 const cors = require('cors');
 
@@ -12,9 +16,8 @@ app.use(cors());
 
 const quizzes = require("./db/quizes");
 
-const secretKey = require('secretkey')
 
-
+app.use(express.json()); 
 app.use(express.json());
 
 app.get("/",(req,res)=>{
@@ -25,20 +28,30 @@ app.get("/quiz",(req,res)=>{
     res.json(quizzes.data);
 })
 
-app.post("/login",(req,res)=>{
-    const user={
-        id:1,
-        username: "abhisheksingh",
-        email:"abhishek@gmail.com"
+app.post("/auth/login",(req,res)=>{
+    // const user={
+    //     id:1,
+    //     username: "abhisheksingh",
+    //     email:"abhishek@gmail.com"
+    // }
+    const { username, password } = req.body;
+    const isUserVerified = userdata.users.some(user => user.username === username && user.password === password);
+    if (isUserVerified) {
+        // Use the same token generated during sign-up instead of creating a new one
+        const token = jwt.sign({ id: username }, jwtKey, { expiresIn: '300s' });
+        res.json({
+            token,
+            username,
+            message: "Moj kardi bhai"
+        });
     }
-    jwt.sign({user},secretKey,{expiresIn:"300s"},(err,token)=>{
-res.json({token})
-    })
-
     
-   
+    else {
+        res.status(401).json({ message: "Invalid Credentials" });
+    }})
 
-})
+
+
 
 
 app.listen(PORT,()=>{
